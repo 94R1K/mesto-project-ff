7bfbd42f0258501b62de6288312f2ca43934fc2e
94R1K <dfdfdf.dfdf.1985@mail.ru>
2024-04-06 23:17:44 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,17 @@
 const cardTemplate = document.getElementById('card-template').content;
 const placesList = document.querySelector('.places__list');
 
-function addCard(link, description, deleteCardCallback) {
+function createCard(link, description, deleteCardCallback) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImg = cardElement.querySelector('.card__image');
     const cardDescription = cardElement.querySelector('.card__description');
     const cardDescriptionTitle = cardDescription.querySelector('.card__title');
     const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardImg.alt = `Изображение ${description}`;
     cardImg.src = link;
     cardDescriptionTitle.textContent = description;
     deleteButton.addEventListener('click', () => deleteCardCallback(cardElement));
-    placesList.append(cardElement);
+    return cardElement;
 }
 
 function deleteCard(cardElement) {
@@ -19,6 +20,7 @@ function deleteCard(cardElement) {
 
 document.addEventListener('DOMContentLoaded', function() {
     initialCards.forEach(function (elem) {
-        addCard(elem.link, elem.name, deleteCard);
-    })
+        const cardElement = createCard(elem.link, elem.name, deleteCard);
+        placesList.append(cardElement);
+    });
 });
